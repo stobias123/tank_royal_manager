220aa2261dfbff041e7988cd9b1539c2392dae62
Steven Tobias <stobias123@gmail.com>
2022-10-31 09:23:19 -0700

    setup py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tank_royal_manager',
+    version='',
+    packages=['tank_royal_manager', 'tank_royal_manager.manager', 'tank_royal_manager.robocode_event_models'],
+    url='',
+    license='',
+    author='steven.tobias',
+    author_email='',
+    description=''
+)
